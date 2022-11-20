4902faf329d3e2d5815c7518b987f45b911f6b90
VILSAN <49275632+Fyri1@users.noreply.github.com>
2022-11-20 20:41:35 +0300

    coments
    coments

--- server/controller/calendar-controller.js
+++ server/controller/calendar-controller.js
@@ -1,7 +1,7 @@
 class Calendar {
   addEventCalendar(req, res, next) {
-    console.log(req.body);
-    res.end('OK');
+    console.log(req.body); // название ивента, дата( на весь дениь или на промедуток), цвет должен быть , начало и конец 
+    res.end('OK');//ok
   }
 }
 
