d4ed8f4e66585574e668e8919c3a040a66b6edaf
Herman <ushkalo.herman@gmail.com>
2022-11-22 04:57:10 +0200

    add routes auth

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,3 +1,11 @@
 const apiPath = 'http://localhost:8080/api';
 
-export default {};
+export default {
+  loginPath: () => [apiPath, 'auth', 'login'].join('/'),
+  registerPath: () => [apiPath, 'auth', 'register'].join('/'),
+  logoutPath: (token) => [apiPath, 'auth', 'logout', token].join('/'),
+  sendMailResetPassword: () => [apiPath, 'auth', 'password-reset'].join('/'),
+  resetPassword: (token) =>
+    [apiPath, 'auth', 'password-reset', token].join('/'),
+  confirmEmail: (token) => [apiPath, 'auth', 'active', token].join('/'),
+};
